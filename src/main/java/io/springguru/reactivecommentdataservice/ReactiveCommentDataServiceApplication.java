1c047177ac6f29919d4e2f5323b9dae57153e6b6
Serdar Dundar <serdar@4play.com.tr>
2018-12-12 13:15:13 +0300

    initial project commit

---
+++ src/main/java/io/springguru/reactivecommentdataservice/ReactiveCommentDataServiceApplication.java
@@ -0,0 +1,12 @@
+package io.springguru.reactivecommentdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCommentDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveCommentDataServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/io/springguru/reactivecommentdataservice/ReactiveCommentDataServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package io.springguru.reactivecommentdataservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveCommentDataServiceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
